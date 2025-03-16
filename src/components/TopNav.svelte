b401c66d450e73364f1c4b07f72a251c53599753
Tony Riggioni <tony.riggioni@gmail.com>
2025-03-15 23:55:30 -0700

    Initial Commit \n I shouldn't have started this so late

---
+++ src/components/TopNav.svelte
@@ -0,0 +1,62 @@
+<script module>
+    import * as m from "../lib/paraglide/messages.js"
+
+    let username = m.LOGIN(); // Titlebar reads as Welcome, Login! before user logs in.
+</script>
+
+<script lang="ts">
+    import type { AvailableLanguageTag } from '$lib/paraglide/runtime';
+    import { i18n } from '$lib/i18n';
+    import { page } from '$app/state';
+    import { goto } from '$app/navigation';
+
+    function switchLang(lang: AvailableLanguageTag) {
+        const canonicalPath = i18n.route(page.url.pathname);
+        const localPath = i18n.resolveRoute(canonicalPath, lang);
+        goto(localPath);
+    }
+</script>
+
+<div class="topnav">
+    <div class="left-align">{m.WELCOME({user: username})}</div>
+    <div class="center-align top-title">{m.PUNNAME()}</div>
+    <button class="right-align" onclick={() => switchLang('en')}>English</button>
+    <button class="right-align" onclick={() => switchLang('es')}>Español</button>
+</div>
+
+<style>
+    .topnav {
+        background-color: #333;
+        overflow: hidden;
+        flex: auto;
+    }
+
+    .topnav button,div {
+        padding: 14px 16px;
+        font-size: 17px;
+        color: #fafafa;
+        border: none;
+        background: #333 none;
+    }
+
+    .topnav .right-align {
+        float: right;
+    }
+
+    .topnav .center-align {
+        position: absolute;
+        float: left;
+        left: 46%;
+        text-align: center;
+    }
+
+    .topnav .left-align {
+        float: left;
+    }
+
+    .topnav .top-title {
+        padding-top: 10px;
+        font-size: 25px;
+    }
+
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+    import * as m from "../lib/paraglide/messages.js"
+    import TopNav from "../components/TopNav.svelte";
+
+    document.title = m.LOGIN() // Workaround for a Svelte bug that causes rendering to crash
+</script>
+<head>
+    <link rel="stylesheet" href="colours.css">
+</head>
+
+<TopNav></TopNav>
+
+<h1>{m.SITE_WELCOME()}</h1>
+<div class="median-centre">
+    <p>{m.PLEASE_LOGIN()}</p>
+</div>
+
+<style>
+    h1 {
+        text-align: center;
+    }
+
+    .median-centre {
+        flex: auto;
+        width: 85%;
+        overflow: auto;
+        align-content: center;
+    }
+</style>
